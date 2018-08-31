acb60a1d15a1f5bde95ac5dc96f15360e8aa42ad
Graham Christensen <graham.christensen@target.com>
2018-08-31 14:13:03 -0400

    Swap a setup.py for a setup.cfg

--- fetch-git/src/setup.py
+++ fetch-git/src/setup.py
@@ -1,17 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name="target-nix-fetchers-fetch-git",
-    version="1.0.0",
-    description=("Fetch git repositories quickly."),
-    license="MIT",
-    keywords="nix, git",
-    url="https://github.com/target/nix-fetchers",
-    packages=find_packages(),
-    py_modules=['fetchgit'],
-    entry_points={
-        'console_scripts': [
-            'fetch-git = fetchgit.fetchgit:main'
-        ]
-    },
-)
+setup()
