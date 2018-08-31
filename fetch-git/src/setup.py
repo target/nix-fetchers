72251dab41d2f2b89e5f877f721abd6fba81bd41
Graham Christensen <graham.christensen@target.com>
2018-08-31 12:48:46 -0400

    fetch-git: init a setup.py for proper packaging

---
+++ fetch-git/src/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="target-nix-fetchers-fetch-git",
+    version="1.0.0",
+    description=("Fetch git repositories quickly."),
+    license="MIT",
+    keywords="nix, git",
+    url="https://github.com/target/nix-fetchers",
+    packages=find_packages(),
+    py_modules=['fetchgit'],
+    entry_points={
+        'console_scripts': [
+            'fetch-git = fetchgit:main'
+        ]
+    },
+)
