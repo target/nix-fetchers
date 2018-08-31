f86f14bd7dadc00d8b5acfb05e3c8563104e3ff8
Graham Christensen <graham.christensen@target.com>
2018-08-31 13:05:42 -0400

    fetch-git: Update build for a proper python package

--- fetch-git/build.nix
+++ fetch-git/build.nix
@@ -1,19 +1,13 @@
-{ runCommand, bash, git, gnutar, python3, python3Packages, makeWrapper }:
+{ bash, git, gnutar, python3, python3Packages, makeWrapper }:
 
-runCommand "fetch-git" {
+python3Packages.buildPythonPackage {
+  pname = "target-nix-fetchers-fetch-git";
+  version = "1.0.0";
+
   src = ./src;
-  inherit git gnutar python3;
-  nativeBuildInputs = [ makeWrapper ];
-}
-''
-    mkdir -p $out/bin $out/lib
 
-    cp $src/*.py $out/lib
-    chmod +x $out/lib/fetch-git.py
-
-    ln -s $out/lib/fetch-git.py $out/bin/fetch-git
-
-    patchShebangs $out/lib/fetch-git.py
+  postFixup = ''
     wrapProgram $out/bin/fetch-git \
-      --set PATH "${python3}/bin:${git}/bin:${gnutar}/bin:${bash}/bin"
-''
+      --set PATH "${git}/bin:${gnutar}/bin:${bash}/bin"
+  '';
+}
